2200d90a17a65fa41bc9d0831301fcd39cd30d2a
Fava24 <vanessamaria.fava@gmail.com>
2020-02-01 00:04:02 -0300

    teste

--- desafio012.js
+++ desafio012.js
@@ -6,7 +6,9 @@ const contribuicao = 50
 const soma = (idade + contribuicao)
 
 if (contribuicao > 30 && sexo == 'F' || contribuicao > 35 && sexo == 'M')
-if (soma >= 85 && sexo = 'F' || soma >= 95 && sexo = 'M')
+if (soma >= 85 && sexo == 'F' || soma >= 95 && sexo == 'M')
 console.log(`${nome}, Você vai se aposentar`)
 else {console.log (`${nome}, Tem que trabalhar mais`)
 }
+
+
